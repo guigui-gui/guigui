68a6304b28e26db7cede1452787a8d8aef784871
Hajime Hoshi <hajimehoshi@gmail.com>
2025-11-08 04:13:03 +0900

    internal/clipboard: add an expiration time for clipboard data
    Reading (and writing) a clipboard data is pretty heavy.

--- internal/clipboard/clipboard.go
+++ internal/clipboard/clipboard.go
@@ -10,32 +10,34 @@ import (
 )
 
 var (
-	cachedClipboardData     atomic.Value
-	cachedClipboardDataTime atomic.Int64
+	cachedClipboardData           atomic.Value
+	cachedClipboardDataExpireTime atomic.Int64
 )
 
 func ReadAll() (string, error) {
-	if ebiten.Tick() <= cachedClipboardDataTime.Load() {
+	if ebiten.Tick() <= cachedClipboardDataExpireTime.Load() {
 		v, ok := cachedClipboardData.Load().(string)
 		if !ok {
 			return "", nil
 		}
 		return v, nil
 	}
+	// TODO: Read clipboard data asynchronously.
 	data, err := readAll()
 	if err != nil {
 		return "", err
 	}
 	cachedClipboardData.Store(data)
-	cachedClipboardDataTime.Store(ebiten.Tick())
+	cachedClipboardDataExpireTime.Store(ebiten.Tick() + int64(ebiten.TPS()))
 	return data, nil
 }
 
 func WriteAll(text string) error {
+	// TODO: Write clipboard data asynchronously.
 	if err := writeAll(text); err != nil {
 		return err
 	}
 	cachedClipboardData.Store(text)
-	cachedClipboardDataTime.Store(ebiten.Tick())
+	cachedClipboardDataExpireTime.Store(ebiten.Tick() + int64(ebiten.TPS()))
 	return nil
 }
