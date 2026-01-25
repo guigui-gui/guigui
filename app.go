08685107b52fbfc5016917b3a37d8b43ef713896
Hajime Hoshi <hajimehoshi@gmail.com>
2026-01-26 02:38:05 +0900

    Revert "guigui: ensure that Widget.Draw always accepts an already-drawn image"
    This reverts commit a704c73a1f369231d316905fb03c5485bfb14340.
    
    Reason: Widget.SetOpacity doesn't work correctly
    
    Updates #249

--- app.go
+++ app.go
@@ -765,12 +765,7 @@ func (a *app) doDrawWidget(dst *ebiten.Image, widget Widget, layerToRender int64
 		if useOffscreen {
 			origDst = dst
 			dst = widgetState.ensureOffscreen(dst.Bounds())
-			// Copy the destination image to the offscreen image to ensure that
-			// widget.Draw always accepts an already-drawn image.
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64(dst.Bounds().Min.X), float64(dst.Bounds().Min.Y))
-			op.Blend = ebiten.BlendCopy
-			dst.DrawImage(origDst, op)
+			dst.Clear()
 		}
 		widgetBounds := widgetBoundsFromWidget(&a.context, widget)
 		widget.Draw(&a.context, widgetBounds, dst.SubImage(vb).(*ebiten.Image))
